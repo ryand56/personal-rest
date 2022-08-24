3cd3fb8e5a5e16bc2f894f8839fdf2d6117c9ca1
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 04:32:46 -0600

    Testing

--- routes/spotify.js
+++ routes/spotify.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("*", (req, res) => {
-    res.status(404).json({ status: "NOT_FOUND" });
+    res.status(404).json({ success: false, message: "NOT_FOUND" });
 })
 
 module.exports = router;
