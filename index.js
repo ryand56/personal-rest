b728f5f1d638800ec698017a2fb75fc47934a25e
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 02:07:13 -0600

    Add spotify route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const spotify = require("./routes/spotify");
+app.use("/spotify", spotify);
+
 app.get("/", (req, res) => {
     res.json({ status: "OK" });
 });
