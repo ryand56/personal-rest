3cb87edab6deedaad19376e7f37e267b30c19e97
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 04:22:53 -0600

    Update spotify routes

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("*", (req, res) => {
+    res.status(404).json({ status: "NOT_FOUND" });
+})
+
 module.exports = router;
