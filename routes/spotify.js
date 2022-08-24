985350b34de40a6985b57d2cbf71daedccb2327f
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 16:10:32 -0600

    Add authorize route

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/authorize", (req, res) => {
+    res.status(204).end();
+});
+
 router.get("*", (req, res) => {
     res.status(404).json({ success: false, message: "NOT_FOUND" });
 });
