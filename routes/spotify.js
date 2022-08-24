7be881b7daedcb04fb76e3c8edac650148089f5a
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 16:17:49 -0600

    Add main route to spotify

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.status(204).end();
+});
+
 router.get("/authorize", (req, res) => {
     res.status(204).end();
 });
