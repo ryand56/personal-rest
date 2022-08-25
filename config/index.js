0f9cbce6f0ca3c7cf258c24bd338fa834bcccf18
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 18:56:43 -0600

    Fix export issue

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-export const BaseUrl = process.env.BASE_URL || "http://localhost";
+exports.BaseUrl = process.env.BASE_URL || "http://localhost";
 
-export const SpotifyConfig = {
+exports.SpotifyConfig = {
     clientId: process.env.SPOTIFY_CLIENT_ID || "",
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
     isConfigured: clientId && clientSecret
