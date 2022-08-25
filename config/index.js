5f144fd0433cc8110d56a07384755137ae106329
Ryan Omasta <ryand@emeraldsys.xyz>
2022-08-24 18:52:57 -0600

    Add spotify authorization code flow

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+export const BaseUrl = process.env.BASE_URL || "http://localhost";
+
 export const SpotifyConfig = {
     clientId: process.env.SPOTIFY_CLIENT_ID || "",
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
--- routes/spotify.js
+++ routes/spotify.js
@@ -1,12 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
+const axios = require("axios");
+const fs = require("fs");
+const { BaseUrl, SpotifyConfig } = require("../config");
+
 router.get("/", (req, res) => {
     res.status(204).end();
 });
 
 router.get("/authorize", (req, res) => {
-    res.status(204).end();
+    if (!SpotifyConfig.isConfigured) res.status(500).json({
+        success: false,
+        message: "Not configured"
+    });
+
+    const query = new URLSearchParams({
+        response_type: "code",
+        client_id: SpotifyConfig.clientId,
+        scope: encodeURIComponent("user-read-playback-state user-read-currently-playing"),
+        redirect_uri: `${BaseUrl}/spotify/callback`
+    }).toString();
+
+    res.redirect(`https://accounts.spotify.com/authorize?${query}`);
+});
+
+router.get("/callback", async (req, res) => {
+    const code = req.query.code;
+    if (!code) res.status(400).json({ success: false, message: "Code not provided" });
+
+    const body = new URLSearchParams({
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `${BaseUrl}/spotify/callback`
+    }).toString();
+
+    const ret = await axios.default.post("https://accounts.spotify.com/api/token", body, {
+        headers: {
+            Authorization: `Basic ${Buffer.from(`${SpotifyConfig.clientId}:${SpotifyConfig.clientSecret}`).toString("base64")}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+
+    if (ret.status < 200 || ret.status >= 400) res.status(500).json({ success: false, message: "Token exchange failed" });
+    
+    if (ret.data.access_token && ret.data.refresh_token)
+    {
+        if (!fs.existsSync("./tokenConfig")) fs.mkdirSync("./tokenConfig");
+        fs.writeFileSync("./tokenConfig/.spotify", JSON.stringify({ access: ret.data.access_token, refresh: ret.data.refresh_token }));
+    }
+
+    res.json({ success: true });
 });
 
 router.get("*", (req, res) => {
